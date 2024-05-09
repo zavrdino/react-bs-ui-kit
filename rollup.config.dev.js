d34b8f633478121c594de4295b4aec38e0a8c21a
zavrdino <mail.timetravelstuff@gmail.com>
2024-05-09 19:31:48 -0300

    improve dist packaging

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -12,7 +11,7 @@ export default {
       file: 'dist/index.js',
       format: 'esm',
       exports: 'named',
-      sourcemap: false
+      sourcemap: true
     }
   ],
   plugins: [
@@ -20,12 +19,10 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
-    terser(),
     postcss({
       extract: true,
       modules: true,
       use: ['sass'],
-      minimize: true
     }),
   ],
   external: ['react', 'react-dom'],
